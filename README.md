1f618ff5e7079a66eb21fa18f532f911a4401d87
mwendapeter72 <mwendapeter72@gmail.com>
2018-07-26 08:56:11 +0300

    add more logics

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 * Uncomment the commented lines to download the necessary data, then revert.
 * To install nltk use pip
 * pip install nltk
+* To read more on nlp, see this https://pythonspot.com/category/nltk/ 
 
 
--- nlp.py
+++ nlp.py
@@ -3,26 +3,82 @@ import nltk
 #nltk.download('averaged_perceptron_tagger')
 #nltk.download('maxent_ne_chunker')
 #nltk.download('words')
-#nltk.download()
+#nltk.download('stopwords') 
+#this will download all data 
+#nltk.download() 
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords, names
+from nltk.stem import PorterStemmer
+from nltk.tokenize import PunktSentenceTokenizer 
+
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
 
 
-from nltk import word_tokenize
 
+text = 'Peter is happy and this is a happy day. However, the class was such a boring one.' 
+phrases = sent_tokenize(text)
+words = word_tokenize(text)
 
-text = 'peter is happy, this is a happy day, it was such a boring class' 
-tokens = word_tokenize(text) 
-print(tokens)
-tagged = nltk.pos_tag(tokens)
 print("**************************************")
+print(phrases)
+print("**************************************")
+print(words)
+print("**************************************")
+# tag words, verbs, noun, etc 
+tagged = nltk.pos_tag(words)
 print(tagged)
-entities = nltk.chunk.ne_chunk(tagged)
 print("**************************************")
-print(entities)
+entities = nltk.chunk.ne_chunk(tagged)
+#print(entities)
+#print("**************************************")
 
+stopWords = set(stopwords.words('english'))
+wordsFiltered = []
+ 
+for w in words:
+    if w not in stopWords:
+        wordsFiltered.append(w)
+ 
+print(wordsFiltered)
+print("**************************************")
 
+'''
+ps = PorterStemmer()
+ 
+for word in words:
+    print(ps.stem(word))
+print("**************************************")
+''' 
 
-
-
-
-
-
+def word_feats(words):
+    return dict([(word, True) for word in words])
+ 
+positive_vocab = [ 'awesome', 'outstanding', 'fantastic', 'terrific', 'good', 'nice', 'great', ':)' ]
+negative_vocab = [ 'bad', 'terrible','useless', 'hate', ':(' ]
+neutral_vocab = [ 'movie','the','sound','was','is','actors','did','know','words','not' ]
+ 
+positive_features = [(word_feats(pos), 'pos') for pos in positive_vocab]
+negative_features = [(word_feats(neg), 'neg') for neg in negative_vocab]
+neutral_features = [(word_feats(neu), 'neu') for neu in neutral_vocab]
+ 
+train_set = negative_features + positive_features + neutral_features
+ 
+classifier = NaiveBayesClassifier.train(train_set) 
+ 
+# Predict
+neg = 0
+pos = 0
+sentence = "Awesome movie, I liked it"
+sentence = sentence.lower()
+words = sentence.split(' ')
+for word in words:
+    classResult = classifier.classify( word_feats(word))
+    if classResult == 'neg':
+        neg = neg + 1
+    if classResult == 'pos':
+        pos = pos + 1
+ 
+print('Positive: ' + str(float(pos)/len(words)))
+print('Negative: ' + str(float(neg)/len(words)))
